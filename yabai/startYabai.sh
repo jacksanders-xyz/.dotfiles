9f90e70871f542b2a33f62e4a314f1d80a3084e7
Jack Sanders <Guitarbreakerjs@gmail.com>
2022-01-17 01:40:39 -0500

    polybar switch

--- yabai/startYabai.sh
+++ yabai/startYabai.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 brew services start skhd; brew services start yabai
+open /Volumes/Macintosh\ HD/Applications/Übersicht.app/
+# osascript -e 'launch the application "Übersicht"'
--- yabai/stopYabai.sh
+++ yabai/stopYabai.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 brew services stop skhd; brew services stop yabai
+osascript -e 'quit app "Übersicht"'
