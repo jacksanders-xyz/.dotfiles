d6f62f5fe576a71b36ed4bde8d9662810017d784
Jack Callaway Sanders <Jack.Sanders@ibm.com>
2022-02-25 15:59:46 -0500

    youtube conrol in vim

---
+++ bin/playpause.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+osascript -e 'on run	tell (activate application "Google Chrome")		tell application "System Events" to key code {49}	end tell		tell (activate application "iTerm")	end tellend run'
+
+# osascript -e 'tell (activate application "Google Chrome")	tell application "System Events" to key code {49}end telltell (activate application "iTerm")end tell'
+# osascript -e 'tell (activate application "Google Chrome")	tell application "System Events" to key code {49}end tell'
+# osascript -e "tell application \"iTerm2\"" -e "activate" -e "end tell"
+
+# osascript -e 'tell (activate application "Google Chrome")	tell application "System Events" to key code {49}	activate application "iTerm"end tell'
+
+# osascript -e 'activate application "Google Chrome"tell application "System Events" to key code {49}activate application "iTerm"end'
+# osascript -e "tell application \"iTunes\"" -e "activate" -e "end tell"
+
+
+# osascript -e "activate application \"Google Chrome\"" -e "tell application \"System Events\" to key code {49}" -e 'activate application "iTerm"' -e 'end'
+# osascript -e "tell application \"Google Chrome\"" -e "activate" -e "end tell"
+
+# osascript -e 'activate application "Google Chrome" tell application "System Events" to key code {49} activate application "iTerm" end'
+
+# osascript -e 'activate application "Google Chrome" tell application "System Events" to key code {49} activate application "iTerm" end'
+# osascript -e 'activate application "Google Chrome" tell application "System Events" to key code {49} activate application "iTerm2" end'
+# osascript -e 'activate application "iTerm2" tell application "System Events" to key code {4, 14, 37, 37, 31, 47} end'
