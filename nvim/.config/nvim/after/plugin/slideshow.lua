7ff59efff63241ceb43c7ef37f70b97f6211096f
Jack Callaway Sanders <Jack.Sanders@ibm.com>
2022-10-11 16:11:32 -0400

    simple slideshow

--- nvim/.config/nvim/after/plugin/slideshow.lua
+++ nvim/.config/nvim/after/plugin/slideshow.lua
@@ -6,7 +6,6 @@ local api = vim.api
 -- START UP A SLIDESHOW
 -- IF YOU AR GOING TO DO WITH ZEN MODE, DO THAT FIRST
 nnoremap("<leader>wk", function()
-    local currPath = vim.fn.expand('%:p')
-    local command = 'KKSlider '..currPath
+    local command = 'KKSlider '..vim.fn.expand('%:p')
     api.nvim_command(command)
 end)
