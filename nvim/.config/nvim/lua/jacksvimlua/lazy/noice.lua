bbda8dd4428df0486cba42e315da92ea573b3952
Jack Callaway Sanders <Jack.Sanders@ibm.com>
2025-03-11 23:44:18 -0400

    proper messages

--- nvim/.config/nvim/lua/jacksvimlua/lazy/noice.lua
+++ nvim/.config/nvim/lua/jacksvimlua/lazy/noice.lua
@@ -39,8 +39,8 @@ return {
                 messages = {
                     -- NOTE: If you enable messages, then the cmdline is enabled automatically.
                     -- This is a current Neovim limitation.
-                    enabled = true, -- enables the Noice messages UI
-                    view = "mini", -- default view for messages
+                    enabled = false, -- enables the Noice messages UI
+                    view = "cmdline", -- default view for messages
                     falseview_error = "notify", -- view for errors
                     view_warn = "notify", -- view for warnings
                     view_history = "messages", -- view for :messages
