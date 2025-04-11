f8b5b1d7be3ed9661a981178a6e8834e213c17e9
Jack Callaway Sanders <Jack.Sanders@ibm.com>
2025-04-10 20:27:40 -0400

    better move focus command

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -29,32 +29,3 @@ hs.hotkey.bind({"alt"}, "o", function()
     }
     hs.mouse.setAbsolutePosition(pos)
 end)
-
--- Toggle state: false means dock/menu is not revealed; true means it is.
-local dockRevealed = false
-
-hs.hotkey.bind({"alt"}, "a", function()
-    local currentScreen = hs.mouse.getCurrentScreen()
-    local frame = currentScreen:frame()
-
-    if not dockRevealed then
-        -- First toggle: move to top center (with a slight offset)
-        local pos = {
-            x = frame.x + (frame.w / 2) - 350,  -- center horizontally
-            y = frame.y - 25              -- 25 pixels above the frame’s top edge (to trigger the dock/menu)
-        }
-        hs.mouse.setAbsolutePosition(pos)
-        hs.eventtap.leftClick(pos)  -- click to reveal the dock/menu
-        dockRevealed = true
-    else
-        -- Second toggle: move the mouse to the bottom-right and click
-        local newPos = {
-            x = frame.x + frame.w - 1,    -- right edge of the screen
-            y = frame.y + frame.h - 1     -- bottom edge of the screen
-        }
-        hs.mouse.setAbsolutePosition(newPos)
-        hs.eventtap.rightClick(newPos)
-        hs.eventtap.rightClick(newPos)
-        dockRevealed = false
-    end
-end)
