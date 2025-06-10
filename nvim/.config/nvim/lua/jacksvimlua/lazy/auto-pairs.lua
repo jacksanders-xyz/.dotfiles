17fe48ef8c8342d656addf6bfdd72d507b30ae2a
Jack Callaway Sanders <Jack.Sanders@ibm.com>
2025-06-10 17:04:14 -0400

    auto pairs is really good with cmp

--- nvim/.config/nvim/lua/jacksvimlua/lazy/auto-pairs.lua
+++ nvim/.config/nvim/lua/jacksvimlua/lazy/auto-pairs.lua
@@ -1,7 +1,14 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = true,
-	-- use opts = {} for passing setup options
-	-- this is equivalent to setup({}) function
+	opts = {}, -- your autopairs options
+	config = function(_, opts)
+		-- 1. Standard autopairs setup
+		require("nvim-autopairs").setup(opts)
+
+		-- 2. cmp integration
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		local cmp = require("cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }
--- nvim/.config/nvim/lua/jacksvimlua/lazy/cmp.lua
+++ nvim/.config/nvim/lua/jacksvimlua/lazy/cmp.lua
@@ -1,62 +1,62 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        lazy = false,
-        priority = 100,
-        dependencies = {
-            "onsails/lspkind.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-cmdline",
-            "saadparwaiz1/cmp_luasnip",
-            { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-        },
+	{
+		"hrsh7th/nvim-cmp",
+		lazy = false,
+		priority = 100,
+		dependencies = {
+			"onsails/lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
+			{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+		},
 
-        config = function()
-            local cmp = require('cmp')
-            local cmp_lsp = require("cmp_nvim_lsp")
-            local lspkind = require('lspkind')
-            local capabilities = vim.tbl_deep_extend(
-                "force",
-                {},
-                vim.lsp.protocol.make_client_capabilities(),
-                cmp_lsp.default_capabilities())
+		config = function()
+			local cmp = require("cmp")
+			local cmp_lsp = require("cmp_nvim_lsp")
+			local lspkind = require("lspkind")
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				cmp_lsp.default_capabilities()
+			)
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body) -- for `luasnip` users.
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    -- navigate through the items and insert them without confirming
-                    ['<c-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                    ['<c-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-                    ['<c-y>'] = cmp.mapping.confirm({ select = true }),
-
-            }),
-            sources = {
-                { name = "nvim_lsp" },
-                { name = "path" },
-                { name = "buffer" },
-            },
-            formatting = {
-                format = lspkind.cmp_format({
-                    mode = 'text',
-                    maxwidth = 50,
-                    ellipsis_char = '...',
-                    show_labeldetails = true,
-                    menu = ({
-                        nvim_lsp = "[lsp]",
-                        path = "[path]",
-                       	nvim_lua = "[lua]",
-                        buffer = "[buffer]",
-                        luasnip = "[luasnip]",
-                    }),
-                })
-            }
-        })
-    end,
-  },
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body) -- for `luasnip` users.
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					-- navigate through the items and insert them without confirming
+					["<c-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<c-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<c-y>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+					{ name = "buffer" },
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "text",
+						maxwidth = 50,
+						ellipsis_char = "...",
+						show_labeldetails = true,
+						menu = {
+							nvim_lsp = "[lsp]",
+							path = "[path]",
+							nvim_lua = "[lua]",
+							buffer = "[buffer]",
+							luasnip = "[luasnip]",
+						},
+					}),
+				},
+			})
+		end,
+	},
 }
